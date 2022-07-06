d2409edc6bdc602312a86b205abc840f08bfb5f7
byCrookie <maeni.h01@hotmail.com>
2022-07-06 21:24:20 +0200

    renamed extensions method

--- src/DependencyInjection/Microsoft/Modules/ModuleExtensions.cs
+++ src/DependencyInjection/Microsoft/Modules/ModuleExtensions.cs
@@ -4,7 +4,7 @@ namespace DependencyInjection.Microsoft.Modules;
 
 public static class ModuleExtensions
 {
-    public static IServiceCollection UseModule(this IServiceCollection services, Module module)
+    public static IServiceCollection AddModule(this IServiceCollection services, Module module)
     {
         module.Load(services);
         return services;
